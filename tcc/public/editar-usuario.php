ff6ede3f1dd3532dc7f4d9095030578a38734004
Felipe dos Santos <felipsantonios@gmail.com>
2025-08-01 20:23:21 -0300

    pasta process

--- tcc/public/editar-usuario.php
+++ tcc/public/editar-usuario.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-require_once __DIR__ . '/../models/Usuario.php';
-require_once __DIR__ . '/../models/UsuarioDAO.php';
+require_once __DIR__ . '/.../models/Usuario.php';
+require_once __DIR__ . '/.../models/UsuarioDAO.php';
 
 if (!isset($_SESSION['logado']) || !isset($_SESSION['usuario'])) {
     header('Location: login.php');
